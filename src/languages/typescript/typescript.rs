2969c35bc39d201a3ce814507a222e6117248eb9
Vulpesx <potaytochipgamer@gmail.com>
2024-06-17 18:48:13 +1000

    fix: outdated and wrong confgi for typescript

--- src/languages/typescript/typescript.rs
+++ src/languages/typescript/typescript.rs
@@ -14,16 +14,6 @@ pub fn add_typescript(helix_lang_conf: &PathBuf, npm_folder: &PathBuf) {
     let output_err = output.wait_with_output().unwrap();
     if output_err.status.success() {
         println!("Instalación de `npm install -g typescript typescript-language-server` completa");
-
-        let config_to_add = r#"
-[languages.typescript]
-language-server = [
-  { command = "typescript-language-server", args = "--stdio" }
-]
-"#;
-        if let Err(err) = read(helix_lang_conf, config_to_add) {
-            eprintln!("Error al leer el archivo: {err}");
-        }
     } else {
         let stderr = String::from_utf8_lossy(&output_err.stdout);
         eprintln!("Error al ejecutar el comando:\n{stderr}");
