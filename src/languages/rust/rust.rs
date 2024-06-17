37d269bf4c92204ef1db4202a34e6ff0eeb9a5ae
Vulpesx <potaytochipgamer@gmail.com>
2024-06-17 18:47:54 +1000

    fix: outdated and wrong config for rust

--- src/languages/rust/rust.rs
+++ src/languages/rust/rust.rs
@@ -14,19 +14,8 @@ pub fn add_rust(helix_lang_conf: &PathBuf) {
         println!("Instalación de `rustup component add rust-analyzer` completa");
 
         let config_to_add = r#"
-[[language]]
-name = "rust"
-
 [language-server.rust-analyzer.config.check]
 command = "clippy"
-scope = "source.rust"
-injection-regex = "rust"
-file-types = ["rs"]
-roots = ["Cargo.toml", "Cargo.lock"]
-auto-format = true
-comment-token = "//"
-language-server = { command = "rust-analyzer" }
-indent = { tab-width = 4, unit = "    " }
 "#;
         if let Err(err) = read(helix_lang_conf, config_to_add) {
             eprintln!("Error al leer el archivo: {err}");
