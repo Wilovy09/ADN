20e6eab40e7f6c617dbebe0fc9b67ea9a4f351d4
Vulpesx <potaytochipgamer@gmail.com>
2024-06-17 18:34:06 +1000

    fix: error when there is no languages.toml

--- src/utilities/config_managment.rs
+++ src/utilities/config_managment.rs
@@ -4,9 +4,13 @@ use std::io::{self, Read, Write};
 use std::path::PathBuf;
 
 pub fn read(read_file: &PathBuf, content_lsp: &str) -> io::Result<String> {
-    let mut file = File::open(read_file)?;
-    let mut content = String::new();
-    file.read_to_string(&mut content)?;
+    let content = File::open(read_file)
+        .and_then(|mut file| {
+            let mut content = String::new();
+            file.read_to_string(&mut content)?;
+            Ok(content)
+        })
+        .unwrap_or_else(|_| String::new());
     let content_lsp_to_func = content_lsp;
     match add_config_lsp(read_file, content_lsp_to_func) {
         Ok(_) => println!("Config added correctly."),
